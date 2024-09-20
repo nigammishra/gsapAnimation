94d9df44e7b370597973858d243da0761c4c9f8a
nigam <nigammishra826@gmail.com>
2024-09-20 09:54:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to(".imagecontainer", {
+    ease: Expo.easeInOut,
+    width:"100%",
+    stagger:2
+})
+gsap.to(".text h1", {
+    ease: Expo.easeInOut,
+   top:"0",
+    stagger:2
+})
+gsap.to(".text h1", {
+    delay:2,
+    ease: Expo.easeInOut,
+    top:"-105px",
+    stagger:2
+})
+
